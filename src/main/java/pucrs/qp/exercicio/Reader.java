32b1f1732c93dcd02ffaf855518341f3fed7fa79
Vinícius Teixeira <vinicius.teixeira@somosphi.com>
2019-10-07 22:25:45 -0300

    up

--- src/main/java/pucrs/qp/exercicio/Reader.java
+++ src/main/java/pucrs/qp/exercicio/Reader.java
@@ -1,5 +1,27 @@
 package pucrs.qp.exercicio;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.stream.Stream;
+
 public class Reader {
 
+	public int sumAllFileNumbers() {
+		try (Stream<String> stream = Files.lines(Paths.get("numbers.txt"))) {
+
+			int sum = stream.mapToInt(e -> {
+				String[] splitted = e.split("\\:");
+				return Integer.valueOf(splitted[2]);
+			}).reduce(0, (accumulatedInt, element) -> {
+				return accumulatedInt += element;
+			});
+			
+			return sum;
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return 0;
+	}
 }
---
+++ src/test/java/pucrs/qp/exercicio/ReaderTest.java
@@ -0,0 +1,14 @@
+package pucrs.qp.exercicio;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ReaderTest {
+
+	@Test
+	public void sumAllFileNumbers() {
+		fail("Not yet implemented");
+	}
+
+}
